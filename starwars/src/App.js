344f0844d0cfd752b4729bf371abadf5dd47d92b
Brian Bagrov <Brianbagrov@gmail.com>
2020-06-26 20:24:29 -0400

    Completed the assignment with the styling stretch goal.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+import styled from 'styled-components';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+	// Try to think through what state you'll need for this app before starting. Then build out
+	// the state properties here.
+	const [character, setCharacter] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+	useEffect(() => {
+		axios
+			.get(`https://swapi.dev/api/people`)
+			.then((res) => {
+				console.log(`Resolved: `, res);
+				setCharacter(res.data.results);
+			})
+			.catch((err) => {
+				console.log(`Error: `, err);
+			});
+	}, []);
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
+	// Fetch characters from the API in an effect hook. Remember, any time you have a
+	// side effect in a component, you want to think about which state and/or props it should
+	// sync up with, if any.
+
+	return (
+		<div>
+			<h1 className='Header'>Characters</h1>
+			<div className='App'>
+				{character.map((char, i) => {
+					return (
+						<Character
+							key={i}
+							name={char.name}
+							height={char.height}
+							weight={char.mass}
+							gender={char.gender}
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
 
 export default App;
